988078d44a138a5be7fd3803a6b3c1f93faecc32
regispirard <regis.pirard@tincid.com>
2024-01-22 19:05:48 +0100

    [ADD] total refactor

--- l10n_be_payroll_bik_vehicle/models/fleet_vehicle.py
+++ l10n_be_payroll_bik_vehicle/models/fleet_vehicle.py
@@ -39,6 +39,8 @@ class FleetVehicle(models.Model):
         return ranges
 
     def _get_degressive_periods(self, vehicle):
+        # Compute degressive periods based on vehicle acquisition date
+        # BIK decreases at each vehicle anniversary
         ranges = vehicle._get_degressive_ranges()
         periods = []
         start_date = datetime(
@@ -65,87 +67,10 @@ class FleetVehicle(models.Model):
                 range_start_date = range_end_date + relativedelta(days=1)
         return periods
 
-    def _get_co2_reference_electric(self):
-        # electric: no co2 reference
-        co2_reference = {
-            # (year, reference co2)
-            2012: 0,
-            2013: 0,
-            2014: 0,
-            2015: 0,
-            2016: 0,
-            2017: 0,
-            2018: 0,
-            2019: 0,
-            2020: 0,
-            2021: 0,
-            2022: 0,
-            2023: 0,
-        }
-        return co2_reference
-
-    def _get_co2_reference_diesel(self):
-        # diesel
-        co2_reference = {
-            # (year, reference co2)
-            2012: 95,
-            2013: 95,
-            2014: 93,
-            2015: 91,
-            2016: 89,
-            2017: 87,
-            2018: 86,
-            2019: 88,
-            2020: 91,
-            2021: 84,
-            2022: 75,
-            2023: 67,
-        }
-        return co2_reference
-
-    def _get_co2_reference_other(self):
-        # other fuel types
-        co2_reference = {
-            # (year, reference co2)
-            2012: 115,
-            2013: 116,
-            2014: 112,
-            2015: 110,
-            2016: 107,
-            2017: 105,
-            2018: 105,
-            2019: 107,
-            2020: 111,
-            2021: 102,
-            2022: 91,
-            2023: 82,
-        }
-        return co2_reference
-
-    def _get_co2_reference(self, fuel_type):
-        if fuel_type == "electric":
-            co2_reference = self._get_co2_reference_electric()
-            return co2_reference
-        if fuel_type == "diesel":
-            co2_reference = self._get_co2_reference_diesel()
-            return co2_reference
-        # other fuel types
-        co2_reference = self._get_co2_reference_other()
-        return co2_reference
-
-    def _get_co2_ref_from_year(self, year, co2_reference):
-        if year in co2_reference:
-            co2_ref = co2_reference[year]
-        else:
-            min_year = min(co2_reference.keys())
-            max_year = max(co2_reference.keys())
-            if year < min_year:
-                co2_ref = co2_reference[min_year]
-            else:
-                co2_ref = co2_reference[max_year]
-        return co2_ref
-
-    def _get_degressive_co2_periods(self, degressive_periods, co2_reference):
+    def _get_degressive_co2_periods(
+        self, degressive_periods, fiscal_fuel_type, vehicle_co2
+    ):
+        co2_model = self.env["fleet.vehicle.bik.co2"]
         periods = []
         for deg_start_date, deg_end_date, deg_percentage in degressive_periods:
             if not deg_start_date:
@@ -156,46 +81,44 @@ class FleetVehicle(models.Model):
             yn = deg_start_date.year
             while i > 0:
                 # Year n
-                co2_ref_n = self._get_co2_ref_from_year(yn, co2_reference)
+                co2_rate_n = co2_model._get_co2_rate_fuel_year(
+                    fiscal_fuel_type, vehicle_co2, yn
+                )
                 periods.append(
                     (
                         date(yn, deg_start_date.month, deg_start_date.day),
                         date(yn, 12, 31),
                         deg_percentage,
-                        co2_ref_n,
+                        co2_rate_n,
                     ),
                 )
                 # Year n+1
                 yn1 = yn + 1
-                co2_ref_n1 = self._get_co2_ref_from_year(yn1, co2_reference)
+                co2_rate_n1 = co2_model._get_co2_rate_fuel_year(
+                    fiscal_fuel_type, vehicle_co2, yn1
+                )
                 periods.append(
                     (
                         date(yn1, 1, 1),
                         date(yn1, deg_end_date.month, deg_end_date.day),
                         deg_percentage,
-                        co2_ref_n1,
+                        co2_rate_n1,
                     ),
                 )
                 i = i - 1
                 yn = yn + 1
-        co2_ref_n = self._get_co2_ref_from_year(yn, co2_reference)
+        co2_rate_n = co2_model._get_co2_rate_fuel_year(
+            fiscal_fuel_type, vehicle_co2, yn
+        )
         periods.append(
             (
                 date(yn, deg_start_date.month, deg_start_date.day),
                 False,
                 deg_percentage,
-                co2_ref_n,
+                co2_rate_n,
             ),
         )
         return periods
-
-    def _compute_co2_rate_from_co2_ref(self, co2_ref, co2_vehicle):
-        co2_rate = 0.055 + ((co2_vehicle - co2_ref) * 0.001)
-        if co2_rate > 0.18:
-            co2_rate = 0.18
-        if co2_rate < 0.04:
-            co2_rate = 0.04
-        return co2_rate
 
     @api.depends(
         "acquisition_date",
@@ -219,24 +142,27 @@ class FleetVehicle(models.Model):
                 vehicle.is_bik_be_computed = False
                 return
 
+            co2_model = self.env["fleet.vehicle.bik.co2"]
+
             # Remove all previous computations
             bik_items = [(5, 0)]  # Will unlink all records
 
             # BIK will decrease in time - we get the periods
             degressive_periods = vehicle._get_degressive_periods(vehicle)
 
-            # CO2 rate change every year and depends on fuel_type - get periods
-            co2_reference = vehicle._get_co2_reference(vehicle.fuel_type)
+            # Get fiscal fuel type
+            fiscal_fuel_type = co2_model._get_fiscal_fuel_type(vehicle.fuel_type)
 
-            # Mix degressive and co2 periods
-            periods = vehicle._get_degressive_co2_periods(
-                degressive_periods, co2_reference
+            # Get CO2 reference table for this fuel type
+            # co2_ref_table = co2_model._get_co2_reference_history(fiscal_fuel_type)
+
+            # Get all periods and CO2 data for fuel type
+            periods_data = vehicle._get_degressive_co2_periods(
+                degressive_periods, fiscal_fuel_type, vehicle.co2
             )
 
             # Compute BIK for each period
-            for start_date, end_date, percentage, co2_ref in periods:
-
-                co2_rate = vehicle._compute_co2_rate_from_co2_ref(co2_ref, vehicle.co2)
+            for start_date, end_date, percentage, co2_rate in periods_data:
 
                 amount = vehicle.car_value * percentage * (6 / 7) * co2_rate
 
--- l10n_be_payroll_bik_vehicle/models/fleet_vehicle_bik_co2.py
+++ l10n_be_payroll_bik_vehicle/models/fleet_vehicle_bik_co2.py
@@ -1,7 +1,7 @@
 # Copyright 2023 TINCID SRL, Régis Pirard
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class FleetVehicleBikCo2(models.Model):
@@ -14,7 +14,6 @@ class FleetVehicleBikCo2(models.Model):
         selection=[
             ("diesel", "Diesel"),
             ("gasoline", "Gasoline"),
-            ("electric", "Electric"),
         ],
         string="Fuel Type",
         required=True,
@@ -23,3 +22,45 @@ class FleetVehicleBikCo2(models.Model):
         string="Reference CO2",
         required=True,
     )
+
+    @api.model
+    def _get_fiscal_fuel_type(self, vehicle_fuel_type):
+        match vehicle_fuel_type:
+            case "diesel" | "plug_in_hybrid_diesel":
+                return "diesel"
+            case "gasoline" | "plug_in_hybrid_gasoline" | "full_hybrid" | "lpg" | "cng":
+                return "gasoline"
+            case _:
+                return "electric"
+
+    @api.model
+    def _get_co2_reference_history(self, fiscal_fuel_type):
+        return self.search([("fiscal_fuel_type", "=", fiscal_fuel_type)])
+
+    def _get_co2_rate(self, vehicle_co2):
+        # Return CO2 rate for a give vehicle
+        co2_rate = 0.055 + ((vehicle_co2 - self.ref_co2) * 0.001)
+        if co2_rate > 0.18:
+            co2_rate = 0.18
+        if co2_rate < 0.04:
+            co2_rate = 0.04
+        return co2_rate
+
+    @api.model
+    def _get_co2_rate_fuel_year(self, fiscal_fuel_type, vehicle_co2, year):
+        if fiscal_fuel_type == "electric":
+            co2_rate = 0.04
+        else:
+            ref_history = self._get_co2_reference_history(fiscal_fuel_type)
+            # check if year is configured
+            ref_years = ref_history.mapped("year")
+            if year not in ref_years:
+                if year < min(ref_years):
+                    year = min(ref_years)
+                else:
+                    year = max(ref_years)
+            # get ref_co2
+            ref_co2 = ref_history.filtered_domain([("year", "=", year)])
+            # get rate
+            co2_rate = ref_co2._get_co2_rate(vehicle_co2)
+        return co2_rate
